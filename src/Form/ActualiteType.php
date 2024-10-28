3e9585b38f55c524d22db9b2ec2eb2bef22cdfb7
Mathys Langiny <mathys.langiny@gmail.com>
2024-10-28 16:05:07 +0100

    Création du formulaire pour ajouter des actualités

---
+++ src/Form/ActualiteType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Actualite;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ActualiteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class)
+            ->add('contenu', TextareaType::class)
+            ->add('datePublication', DateTimeType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Actualite::class,
+        ]);
+    }
+}
