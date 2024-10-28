5f21902877111393e19f9cd80411a1efa30d47a6
Mathys Langiny <mathys.langiny@gmail.com>
2024-10-28 16:23:14 +0100

    Modification de la route de / pour pouvoir afficher les différentes actualities

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Actualite;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-        return $this->render('home/index.html.twig');
+        $actualites = $em->getRepository(Actualite::class)->findBy([], ['datePublication' => 'DESC'], 5);
+
+        return $this->render('home/index.html.twig', [
+            'actualites' => $actualites,
+        ]);
     }
 }
