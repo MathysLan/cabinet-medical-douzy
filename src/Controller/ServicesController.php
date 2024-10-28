126c1dcd0d4d6c4217f0e992cebd63e67b4aa9d1
Mathys Langiny <mathys.langiny@gmail.com>
2024-10-28 15:58:46 +0100

    Ajout des différentes routes pour les différents services

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -13,4 +13,34 @@ class ServicesController extends AbstractController
     {
         return $this->render('services/index.html.twig');
     }
+
+    #[Route('/services/medecin', name: 'app_services_medecin')]
+    public function medecin(): Response
+    {
+        return $this->render('services/medecin.html.twig');
+    }
+
+    #[Route('/services/infirmiere', name: 'app_services_infirmiere')]
+    public function infirmiere(): Response
+    {
+        return $this->render('services/infirmiere.html.twig');
+    }
+
+    #[Route('/services/orthophoniste', name: 'app_services_orthophoniste')]
+    public function orthophoniste(): Response
+    {
+        return $this->render('services/orthophoniste.html.twig');
+    }
+
+    #[Route('/services/psychologue', name: 'app_services_psychologue')]
+    public function psychologue(): Response
+    {
+        return $this->render('services/psychologue.html.twig');
+    }
+
+    #[Route('/services/dentiste', name: 'app_services_dentiste')]
+    public function dentiste(): Response
+    {
+        return $this->render('services/dentiste.html.twig');
+    }
 }
